f9588910a9189b5c0311bd7b8c38fea45d25cdce
Anna Tillack <anna.n.tillack@gmail.com>
2022-12-19 13:54:22 -0500

    added react-router-dom and dom

--- src/App.js
+++ src/App.js
@@ -1,46 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import ReactAutocomplete from 'react-autocomplete';
-import {useSearch, useDebounce} from './hooks';
-import Input from './components/Input';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Search from './pages/Search';
+import notFound from './pages/notFound';
 
 
-function App() {
-  const [value, setValue] = useState('');
-  
 
-  const {articles} = useSearch(useDebounce(value));
+const App = ()  => {
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/search" component={Search}/>
+      <Route component={notFound}/>
+    </Switch>
 
-  
-
-  return (              
-
-    <ReactAutocomplete
-      items={articles}
-      renderInput={Input}
-      inputProps={{placeholder: 'Input a search term:'}}
-      getItemValue={item => item.label}
-      renderMenu={(children, value, style) => (
-        <div style={{...style}} className="input-suggestions">
-          {children}
-          <a href={`/search?query=${value}`} className="search-link">
-           See all results
-          </a>
-        </div>
-      )}
-      renderItem={(item, highlighted) =>
-        <div
-          key={item.id}
-          style={{ backgroundColor: highlighted ? '#eee' : 'transparent'}}
-        >
-          {item.label}
-        </div>
-      }
-      value={value}
-      onChange={e => setValue(e.target.value )}
-      onSelect={value => setValue(value)}
-    />
-
-  )
+  </Router>
 }
 
 export default App;
--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,4 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState, useRef, useCallback } from 'react';
 import axios from 'axios';
 
 export const useSearch = (query) => {
@@ -75,3 +75,12 @@ export const useDebounce = (value, delay = 500 ) => {
     return debouncedValue;
 
 }
+
+export const useSearchForm = () => {
+    const [searchValue, setSearchValue] = useState('');
+    const onSearchChange =  useCallback((e) => setSearchValue(e.target.value), []);
+    return {
+        searchValue, 
+        onSearchChange
+    }
+};
